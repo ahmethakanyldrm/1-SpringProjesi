1477d95ac20bdab36b10e2fbcf7310e3ff500e25
Ahmet Hakan YILDIRIM <hakan-336-ahmet@outlook.com>
2026-04-14 01:45:49 +0300

    Bean Lombok Maven Spring Context

---
+++ src/main/java/com/ahmethakan/entity/User2.java
@@ -0,0 +1,13 @@
+package com.ahmethakan.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class User2 {
+
+    private String name;
+}
---
+++ src/main/java/com/ahmethakan/services/UserService.java
@@ -0,0 +1,14 @@
+package com.ahmethakan.services;
+
+import com.ahmethakan.entity.User;
+import com.ahmethakan.entity.User2;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class UserService {
+    private List<User2> userList;
+}
